fa900c02861e2935cf2988c696eecf36e18caf01
willemijnw <willemijnw@5f2b72d7-f913-0410-b7ee-f4ba798c8f3b>
2017-01-08 19:04:42 +0000

    [maven-release-plugin]  copy for tag unitils-3.4.4

---
+++ unitils-3.4.4/unitils-spring/src/main/java/org/unitils/spring/enums/LoadTime.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) Smals
+ */
+package org.unitils.spring.enums;
+
+
+/**
+ * Should the {@link org.unitils.spring.annotation.SpringApplicationContext} be loaded once or for each class or for each method?.
+ *
+ * @author wiw
+ *
+ * @since 3.4.3
+ *
+ */
+public enum LoadTime {
+   CLASS, METHOD;
+}
